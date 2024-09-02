f6959d49883ae078569e7c92b135af8e4f5f4ed3
Ali Rivera Guerra <aaarrrggg@Alis-MacBook-Pro.local>
2024-09-02 02:51:51 -0400

    Add blurred image overlay effect

---
+++ components/Cards/BlurredImageCard/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.css"
+
+// BlurredImageCard component for static image
+const BlurredImageCard = () => {
+  return (
+    <>
+      <div className={`${styles.flyer} relative max-w-[400px] mx-auto`}>
+        <img
+          className={`relative object-cover w-full h-full mt-4 z-[2]`}
+          src={"/images/flyer_berlin.jpg"}
+          alt="Flyer"
+          title="Flyer"
+        />
+      </div>
+    </>
+  )
+}
+
+export default BlurredImageCard
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from "react"
 import { DocsThemeConfig } from "nextra-theme-docs"
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Ninjix</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/ninjasweb/ninjix",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/ninjasweb/ninjix",
   footer: {
-    text: "Nextra Docs Template",
+    text: "Ninjix by Ninjasweb",
   },
 }
 
