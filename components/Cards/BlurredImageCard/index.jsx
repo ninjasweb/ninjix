c0e6ecee97de9fd01fea74f623d0d83c34f89c7f
Ali Rivera Guerra <aaarrrggg@Alis-MacBook-Pro.local>
2024-09-03 07:54:00 -0500

    Add border-radius to image blurred card

--- components/Cards/BlurredImageCard/index.jsx
+++ components/Cards/BlurredImageCard/index.jsx
@@ -6,7 +6,7 @@ const BlurredImageCard = () => {
     <>
       <div className={`${styles.flyer} relative max-w-[400px] mx-auto`}>
         <img
-          className={`relative object-cover w-full h-full mt-4 z-[2]`}
+          className={`relative object-cover w-full h-full mt-4 z-[2] rounded-md`}
           src={"/images/flyer_berlin.jpg"}
           alt="Flyer"
           title="Flyer"
